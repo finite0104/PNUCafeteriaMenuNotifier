5639eb9e94ce7d07b9832e9910b55f5b9887f85e
finite0104 <how3265@naver.com>
2018-12-27 14:03:56 +0900

    Notification Application v0.0.1

---
+++ LunchNotifiationApplication/app/src/main/java/park_sh/noti/lunch/lunchnotifiationapplication/MainActivity.kt
@@ -0,0 +1,12 @@
+package park_sh.noti.lunch.lunchnotifiationapplication
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
