ecaacc842ef249ae5636925799d06c502ef84046
finite0104 <how3265@naver.com>
2019-01-14 14:46:58 +0900

    adding crawling error exception processing code

--- WebCrawler/crawler.py
+++ WebCrawler/crawler.py
@@ -5,37 +5,40 @@ from time import sleep
 import pymongo
 
 def func_crawling() :
-	options = Options()
-	options.set_headless(True)
-	driver = webdriver.Firefox(options=options, executable_path='./geckodriver')
-	driver.get('http://www.pusan.ac.kr/kor/CMS/MenuMgr/menuListOnWeekly.do?mCode=MN203')
+	try :
+		options = Options()
+		options.set_headless(True)
+		driver = webdriver.Firefox(options=options, executable_path='./geckodriver')
+		driver.get('http://www.pusan.ac.kr/kor/CMS/MenuMgr/menuListOnWeekly.do?mCode=MN203')
 
-	html = driver.page_source
-	soup = BeautifulSoup(html)
-	date_string = soup.find_all("span", {"class": "loca"})[0].text
-	date_number = soup.find_all("span", {"class": "term"})[0].text
-	school_meal_table = soup.find_all("table", {"class": "menu-tbl"})[0]
-	table_body = school_meal_table.find_all("tbody")[0]
-	table_datas = table_body.find_all("tr")
+		html = driver.page_source
+		soup = BeautifulSoup(html)
+		date_string = soup.find_all("span", {"class": "loca"})[0].text
+		date_number = soup.find_all("span", {"class": "term"})[0].text
+		school_meal_table = soup.find_all("table", {"class": "menu-tbl"})[0]
+		table_body = school_meal_table.find_all("tbody")[0]
+		table_datas = table_body.find_all("tr")
 
-	for row in table_datas :
-		counter = 0
-		location = row.find_all("th")[0].text
-		meal_list = row.find_all("td")
-		for data in meal_list :
-			menu_data = data.find_all("li")
-			if(len(menu_data) == 0) :
-				result = "데이터 없음"
-				non_menu_data_insert(date_string, date_number, location, counter, result)
-			else :
-				for data in menu_data :
-					name = data.h3.text
-					menu = data.p.text
-					menu_data_insert(date_string, date_number, location, counter, name, menu)
+		for row in table_datas :
+			counter = 0
+			location = row.find_all("th")[0].text
+			meal_list = row.find_all("td")
+			for data in meal_list :
+				menu_data = data.find_all("li")
+				if(len(menu_data) == 0) :
+					result = "데이터 없음"
+					non_menu_data_insert(date_string, date_number, location, counter, result)
+				else :
+					for data in menu_data :
+						name = data.h3.text
+						menu = data.p.text
+						menu_data_insert(date_string, date_number, location, counter, name, menu)
 
-			counter = counter + 1
-
-	driver.quit()
+				counter = counter + 1
+	except :
+		print('Crawling Error Occured')
+	finally :
+		driver.quit()
 
 def count_to_time(count) :
 	return {
@@ -48,7 +51,7 @@ def count_to_time(count) :
 def non_menu_data_insert(date_string, date_number, location, count, result) :
 	conn = pymongo.MongoClient("localhost", 27017)
 	db = conn.meal_data
-	collection = db.getCollection(date_number + '')
+	collection = db.date_number
 
 	time = count_to_time(count)
 	if count == 0 :
@@ -90,7 +93,7 @@ def non_menu_data_insert(date_string, date_number, location, count, result) :
 def menu_data_insert(date_string, date_number, location, count, name, menu) :
 	conn = pymongo.MongoClient("localhost", 27017)
 	db = conn.meal_data
-	collection = db.getCollection(date_number + '')
+	collection = db.date_number
 
 	time = count_to_time(count)
 	if count == 0 :
