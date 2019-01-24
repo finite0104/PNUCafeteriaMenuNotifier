e3f1e883b09d9445f3e7784dfd1052ebef6deacb
finite0104 <how3265@naver.com>
2019-01-24 14:34:48 +0900

    Router에서 DB처리하지않고, 나누어 구성

---
+++ SchoolMealServer/api/MealData.js
@@ -0,0 +1,18 @@
+var config = require('../config')
+var express = require('express')
+var router = express.Router()
+var dbConnector = require('../db/MealDatabase')
+
+router.get('/', function(req, res) {
+
+})
+
+router.post('/', function(req, res) {
+
+})
+
+router.delete('/:token', function(req, res) {
+
+})
+
+module.exports = router
