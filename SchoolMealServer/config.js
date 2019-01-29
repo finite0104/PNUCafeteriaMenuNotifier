2dc8e8e121920376127c00102bc715efd6cb157c
finite0104 <how3265@naver.com>
2019-01-29 20:13:19 +0900

    PORT 재설정

--- SchoolMealServer/config.js
+++ SchoolMealServer/config.js
@@ -1,5 +1,5 @@
 var config = {
-    "PORT" : "27836",
+    "PORT" : "28300",
     "MONGO_URI_CLIENTS_ID" : "mongodb://localhost/user_ids",
     "MONGO_URI_MEAL_DATA" : "mongodb://localhost/meal_data"
 }
