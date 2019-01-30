23d4a511922e231c415605c4e96c011c9be4bb43
finite0104 <how3265@naver.com>
2019-01-30 16:26:23 +0900

    현재 서버 날짜 가져오는 함수 추가

--- SchoolMealServer/api/MealData.js
+++ SchoolMealServer/api/MealData.js
@@ -3,12 +3,21 @@ var express = require('express')
 var router = express.Router()
 var dbConnector = require('../db/MealDatabase')
 
-router.get('/:date', function(req, res, next) {
-    var requestDateValue = req.params.date
+router.get('/', function(req, res, next) {
+    var now = new Date(Date.now())
+    var dateString = getDateString(now)
 
-    dbConnector.getPNUMealDataForDay(requestDateValue, function(result) {
+    dbConnector.getPNUMealDataForDay(dateString, function(result) {
         res.send(result)
     })
 })
+
+function getDateString(now) {
+    var date  = now.getDate()
+    var month = now.getMonth() + 1
+    var year  = now.getFullYear()
+
+    return '' + year + '.' + (month <= 9 ? '0' + month : month) + '.' + (date <= 9 ? '0' + date : date)
+}
 
 module.exports = router
