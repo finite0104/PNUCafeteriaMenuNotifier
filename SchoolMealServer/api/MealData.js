5be1fbe456e2d15aad9613010b4ec227a50fdff2
finite0104 <how3265@naver.com>
2019-01-30 16:29:44 +0900

    API 데이터 요청 시, URL에 날짜까지 담아서 요청하도록 변경

--- SchoolMealServer/api/MealData.js
+++ SchoolMealServer/api/MealData.js
@@ -4,21 +4,11 @@ var router = express.Router()
 var dbConnector = require('../db/MealDatabase')
 
 router.get('/:date', function(req, res, next) {
-    //var now = new Date(Date.now())
-    //var dateString = getDateString(now)
     var dateString = req.params.date
 
     dbConnector.getPNUMealDataForDay(dateString, function(result) {
         res.send(result)
     })
 })
-
-function getDateString(now) {
-    var date  = now.getDate()
-    var month = now.getMonth() + 1
-    var year  = now.getFullYear()
-
-    return '' + year + '.' + (month <= 9 ? '0' + month : month) + '.' + (date <= 9 ? '0' + date : date)
-}
 
 module.exports = router
