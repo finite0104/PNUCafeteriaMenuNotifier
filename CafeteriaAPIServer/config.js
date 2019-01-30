8187f0d92662295c0d94d0d21900ab4e07a61520
finite0104 <how3265@naver.com>
2019-01-30 16:59:06 +0900

    Renaming 중 발생한 오류현상들 제거

--- CafeteriaAPIServer/config.js
+++
@@ -1,7 +0,0 @@
-var config = {
-    "PORT" : "28300",
-    "MONGO_URI_CLIENTS_ID" : "mongodb://localhost/user_ids",
-    "MONGO_URI_MEAL_DATA" : "mongodb://localhost/meal_data"
-}
-
-module.exports = config
