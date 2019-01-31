eb309595bba5850a4199ea31e3771ef0e55bc5ee
finite0104 <how3265@naver.com>
2019-01-31 19:52:37 +0900

    FCM 처리 기능 추가작업 진행중..

--- WebCrawler/FCMManager.py
+++ WebCrawler/FCMManager.py
@@ -1,5 +1,19 @@
-
+import MongoDBManager
+import json
+from firebase_admin import messaging
 
 class FCM_Manager :
+
+    def get_fcm_key(self) :
+        with open('config.json', 'r') as file :
+            config = json.load(file)
+        return config["FCM"]["API_KEY"]
+
+    def get_device_tokens(self) :
+        return MongoDBManager.get_push_device_tokens()
+
     def __init__(self) :
         #Firebase Cloud Messaging Setting
+        self.api_key = self.get_fcm_key()
+        self.device_tokens = self.get_device_tokens()
+
