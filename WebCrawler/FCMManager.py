fd759b76aca17e35b4466b4fa9207c0af7890037
finite0104 <how3265@naver.com>
2019-01-31 20:42:30 +0900

    Function Encapsulation

--- WebCrawler/FCMManager.py
+++ WebCrawler/FCMManager.py
@@ -3,17 +3,16 @@ import json
 from firebase_admin import messaging
 
 class FCM_Manager :
-
-    def get_fcm_key(self) :
+    def _get_fcm_key(self) :
         with open('config.json', 'r') as file :
             config = json.load(file)
         return config["FCM"]["API_KEY"]
 
-    def get_device_tokens(self) :
+    def _get_device_tokens(self) :
         return MongoDBManager.get_push_device_tokens()
 
     def __init__(self) :
         #Firebase Cloud Messaging Setting
-        self.api_key = self.get_fcm_key()
-        self.device_tokens = self.get_device_tokens()
+        self.api_key = self._get_fcm_key()
+        self.device_tokens = self._get_device_tokens()
 
