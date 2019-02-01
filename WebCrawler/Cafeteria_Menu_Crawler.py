856c9178b03418875c964f81c91cc4a362c3803c
finite0104 <how3265@naver.com>
2019-02-01 16:24:03 +0900

    print 에러 문제 해결

--- WebCrawler/Cafeteria_Menu_Crawler.py
+++ WebCrawler/Cafeteria_Menu_Crawler.py
@@ -7,7 +7,7 @@ if __name__ == "__main__" :
         WebCrawler.pnu_web_crawling()
         fcm_manager = FCMManager.FCM_Manager()
         msg_send_result = fcm_manager.send_fcm_message()
-        print msg_send_result
+        print (msg_send_result)
 
         delay_time = 24 * 60 * 60
         time.sleep(delay_time)
