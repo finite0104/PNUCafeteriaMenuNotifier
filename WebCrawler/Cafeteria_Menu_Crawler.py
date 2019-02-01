f1415d07bc2b21abb6f6967b777691295d6e453c
finite0104 <how3265@naver.com>
2019-02-01 16:49:17 +0900

    스케줄러 설정해 동작하도록 개발 예정

--- WebCrawler/Cafeteria_Menu_Crawler.py
+++ WebCrawler/Cafeteria_Menu_Crawler.py
@@ -9,5 +9,9 @@ if __name__ == "__main__" :
         msg_send_result = fcm_manager.send_fcm_message()
         print (msg_send_result)
 
+        """
+        단순 sleep 함수로 동작하도록 설정하지 않고 
+        스케줄러 구현해서 구동되도록 설정
+        """
         delay_time = 24 * 60 * 60
         time.sleep(delay_time)
