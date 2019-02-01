1d9ff36d13534408681b9ce1a87616abb8be21f9
finite0104 <how3265@naver.com>
2019-02-01 16:14:50 +0900

    FCM Message 여러 유저에게 전송하는 작업 개발 중

--- WebCrawler/FCMManager.py
+++ WebCrawler/FCMManager.py
@@ -1,6 +1,6 @@
 import MongoDBManager
 import json
-from firebase_admin import messaging
+from pyfcm import FCMNotification
 
 class FCM_Manager :
     def _get_fcm_key(self) :
@@ -15,4 +15,13 @@ class FCM_Manager :
         #Firebase Cloud Messaging Setting
         self.api_key = self._get_fcm_key()
         self.device_tokens = self._get_device_tokens()
+        self.push_service = FCMNotification(api_key=self.api_key)
 
+    def send_fcm_message(self) :
+        result = self.push_service.notify_multiple_devices(
+            registration_ids=self.device_tokens,
+            message_title='test_title',
+            message_body='test_message',
+        )
+
+        print result
