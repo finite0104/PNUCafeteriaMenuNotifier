081661241643deddf10c4d51647d29b5f2197aa0
finite0104 <how3265@naver.com>
2019-02-08 18:47:52 +0900

    Firebase Messaging 기능 개발 진행 - 신규 Token 확인/서버로부터 전송되는 FCM Push 메세지 인식 시 처리

---
+++ PNUCafeteriaMenuNotifierApplication/app/src/main/java/parksh/cafeteria/menu/noti/pnucafeteriamenunotifierapplication/FirebaseMessagingService.kt
@@ -0,0 +1,56 @@
+package parksh.cafeteria.menu.noti.pnucafeteriamenunotifierapplication
+
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.media.RingtoneManager
+import android.support.v4.app.NotificationCompat
+import android.util.Log
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+
+class FirebaseMessagingService : FirebaseMessagingService() {
+    private val MESSAGING_TAG = "MessagingService"
+
+    override fun onNewToken(token: String?) {
+        /*
+            토큰 발행 시 호출되는 함수
+            토큰 발행 후 서버 연결
+             -> 서버로 토큰 데이터 전송하도록 기능 개발
+        */
+        Log.d(MESSAGING_TAG, "New Token : $token")
+    }
+
+    override fun onMessageReceived(message: RemoteMessage) {
+        //메세지 수신에 대한 처리
+        Log.d(MESSAGING_TAG, "From: ${message.from}")
+
+        if(message.notification != null) {
+            Log.d(MESSAGING_TAG, "Notification Message Body : ${message.notification?.body}")
+            sendNotification(message.notification?.body)
+        }
+    }
+
+    private fun sendNotification(messageBody: String?) {
+        val intent = Intent(this, MainActivity::class.java).apply {
+            flags = Intent.FLAG_ACTIVITY_CLEAR_TOP
+            putExtra("Notification", messageBody)
+        }
+
+        var pendingIntent = PendingIntent.getActivity(this, 0,
+                                                        intent, PendingIntent.FLAG_ONE_SHOT)
+        val notificationSound = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
+
+        var notificationBuilder = NotificationCompat.Builder(this, "Notification")
+                                                            .setSmallIcon(R.mipmap.ic_launcher)
+                                                            .setContentTitle("PNU Cafeteria Menu")
+                                                            .setContentText(messageBody)
+                                                            .setAutoCancel(true)
+                                                            .setSound(notificationSound)
+                                                            .setContentIntent(pendingIntent)
+
+        var notiManager: NotificationManager = this.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        notiManager.notify(0, notificationBuilder.build())
+    }
+}
