2a384218c117138cec38ec9b90c4c7fbdac4ba39
finite0104 <how3265@naver.com>
2019-02-08 14:31:10 +0900

    Android Application - Project Name Replaced

---
+++ PNUCafeteriaMenuNotifierApplication/app/src/main/java/parksh/cafeteria/menu/noti/pnucafeteriamenunotifierapplication/MainActivity.kt
@@ -0,0 +1,12 @@
+package parksh.cafeteria.menu.noti.pnucafeteriamenunotifierapplication
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
