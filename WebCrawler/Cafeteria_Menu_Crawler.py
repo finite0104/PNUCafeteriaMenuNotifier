e37a34d4e68a8f822d8c3b8489120ee631199e77
박석현 <parkseokhyun@bagseoghyeon-ui-MacBookAir.local>
2020-08-10 03:56:52 +0900

    Crawler Scheduling with APScheduler Library

--- WebCrawler/Cafeteria_Menu_Crawler.py
+++ WebCrawler/Cafeteria_Menu_Crawler.py
@@ -2,17 +2,31 @@
 import WebCrawler
 import FCMManager
 import time
+from apscheduler.schedulers.blocking import BlockingScheduler
+
+def timed_job():
+    print('Test Interval')
+
+def menu_crawling() :
+    WebCrawler.pnu_web_crawling()
+    msg_send_result = fcm_manager.send_fcm_message()
+    print('Menu Crawllllllling~~~')
+
+def mainInit() :
+    scheduler.add_job(timed_job, 'interval', seconds=30)
+    #6시간마다 크롤링 진행할 수 있도록 프로그램 작성
+    scheduler.add_job(menu_crawling, 'interval', hours=6)
+    scheduler.start()
 
 if __name__ == "__main__" :
-    while(True) :
-        WebCrawler.pnu_web_crawling()
-        fcm_manager = FCMManager.FCM_Manager()
-        msg_send_result = fcm_manager.send_fcm_message()
-        print (msg_send_result)
-
-        """
-        단순 sleep 함수로 동작하도록 설정하지 않고 
-        스케줄러 구현해서 구동되도록 설정
-        """
-        delay_time = 24 * 60 * 60
-        time.sleep(delay_time)
+    scheduler = BlockingScheduler()
+    fcm_manager = FCMManager.FCM_Manager()
+    
+    try :
+        while(True) :
+            # Main Thread가 죽지않도록 주기적인 sleeping
+            #delay_time = 24 * 60 * 60
+            delay_time = 5
+            time.sleep(delay_time)
+    except (KeyboardInterrupt, SystemExit) :
+        scheduler.shutdown()
