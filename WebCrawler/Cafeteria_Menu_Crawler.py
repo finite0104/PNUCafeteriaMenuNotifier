d844c2a372abe9d8a5e197ec24ba31557133e06f
finite0104 <parkseokhyun@bagseoghyeon-ui-MacBookAir.local>
2020-08-19 15:50:26 +0900

    Scheduler Update - BlockScheduler --> BackgroundScheduler

--- WebCrawler/Cafeteria_Menu_Crawler.py
+++ WebCrawler/Cafeteria_Menu_Crawler.py
@@ -2,31 +2,44 @@
 import WebCrawler
 import FCMManager
 import time
-from apscheduler.schedulers.blocking import BlockingScheduler
+from apscheduler.schedulers.background import BackgroundScheduler
 
-def timed_job():
-    print('Test Interval')
-
+# 스케줄러가 함수 실행 이후 -> 스케줄 대기 -> 재실행 순서가 아니고 스케줄 대기 -> 실행 순서임
+# 함수에 대한 우선적인 실행 작업이 필요하다.
+# thread 사용이 필요한지 확인해야함
 def menu_crawling() :
     WebCrawler.pnu_web_crawling()
     msg_send_result = fcm_manager.send_fcm_message()
-    print('Menu Crawllllllling~~~')
+    print('Menu Crawling Completed')
 
-def mainInit() :
-    scheduler.add_job(timed_job, 'interval', seconds=30)
-    #6시간마다 크롤링 진행할 수 있도록 프로그램 작성
-    scheduler.add_job(menu_crawling, 'interval', hours=6)
+def scheduling_test_30interval() :
+    print('Scheduling - 30 Seconds')
+
+def scheduling_test_10interval() :
+    print('Scheduling - 10 Seconds')
+
+def schedulerInitialize() :
+    scheduler = BackgroundScheduler()
     scheduler.start()
+    # 6시간마다 크롤링 진행할 수 있도록 프로그램 작성함
+    scheduler.add_job(menu_crawling, 'interval', hours=6)
+
+    # 테스트용 스케줄링 작업
+    scheduler.add_job(scheduling_test_10interval, 'interval', seconds=10)
+    scheduler.add_job(scheduling_test_30interval, 'interval', seconds=30)
 
 if __name__ == "__main__" :
-    scheduler = BlockingScheduler()
+    scheduler = None
+    schedulerInitialize()
     fcm_manager = FCMManager.FCM_Manager()
     
     try :
         while(True) :
             # Main Thread가 죽지않도록 주기적인 sleeping
             #delay_time = 24 * 60 * 60
-            delay_time = 5
+            delay_time = 3
             time.sleep(delay_time)
     except (KeyboardInterrupt, SystemExit) :
-        scheduler.shutdown()
+        if scheduler is not None :
+            # scheduler is alive --> scheduler shutdown
+            scheduler.shutdown()
